fe339c96eeb34ea477185ce9afaa0d25f22f1b47
Sergionx <sergio180203@gmail.com>
2024-01-30 20:59:45 -0400

    Add duration constants for minutes, hours

--- src/Utils/Constants.java
+++ src/Utils/Constants.java
@@ -9,5 +9,8 @@ package Utils;
  * @author Sergionx
  */
 public class Constants {
-        public static final int DAY_DURATION = 24;
+
+    public static final int MINUTE_DURATION = 1000; //TODO Pensar valor. Recordar que sleep es miliseconds
+    public static final int HOUR_DURATION = MINUTE_DURATION * 60;
+    public static final int DAY_DURATION = HOUR_DURATION * 24;
 }
