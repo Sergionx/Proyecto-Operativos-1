cc4d0d8a7560f118b130c924c8120b7849de4712
Sergionx <sergio180203@gmail.com>
2024-02-04 13:00:21 -0400

    Add TrabajadorEstudio and update initalizeEmpleados method

--- src/Empresa/Empresa.java
+++ src/Empresa/Empresa.java
@@ -7,7 +7,9 @@ package Empresa;
 import Trabajadores.Contador;
 import Trabajadores.Director;
 import Trabajadores.ProjectManager;
+import Trabajadores.TipoTrabajador_Estudio;
 import Trabajadores.Trabajador;
+import Trabajadores.TrabajadorEstudio;
 import java.util.concurrent.Semaphore;
 
 /**
@@ -21,12 +23,12 @@ public class Empresa {
     private Director director;
     private Contador contador;
 
-    private Ganancias ganancias;
+    private final Ganancias ganancias;
 
-    private int last_carnet;
+    private final int last_carnet;
     public String nombre;
-    private Drive drive;
-    private Semaphore mutex;
+    private final Drive drive;
+    private final Semaphore mutex;
 
     public Empresa(int last_carnet, String nombre) {
         this.empleados = new Trabajador[last_carnet + 10];
@@ -43,5 +45,26 @@ public class Empresa {
     private void initalizeEmpleados() {
         this.manager = new ProjectManager(this.mutex, this.drive, ganancias, this.contador);
         this.director = new Director(this.mutex, this.drive, ganancias, this.contador, this.manager);
+
+        var trabajador = new TrabajadorEstudio(
+                TipoTrabajador_Estudio.GUIONISTA, mutex, drive,
+                ganancias, last_carnet);
+        trabajador.start();
+        for (int i = 0; i < empleados.length; i++) {
+//            var trabajador = new TrabajadorEstudio(
+//                    TipoTrabajador_Estudio.GUIONISTA, mutex, drive,
+//                    ganancias, last_carnet);
+//            trabajador.start();
+//            this.empleados[i] = trabajador;
+        }
+
+    }
+
+    public int getCapitulosListos(boolean isPlotTwist) {
+        if (isPlotTwist) {
+            return this.drive.capitulos_PlotTwist;
+        } else {
+            return this.drive.capitulos_Estandar;
+        }
     }
 }
